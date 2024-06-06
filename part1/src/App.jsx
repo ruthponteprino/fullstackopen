33b371e5d412b9600ed18423c4907be350262989
ruthponteprino <rponteprino@gmail.com>
2024-06-05 22:09:44 -0300

    agregue comentario

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <h2>dfjdsfsd</h2>
         <p>HOLA MUNDOOOOOOOOOOOO</p>
       </div>
       <p className="read-the-docs">
